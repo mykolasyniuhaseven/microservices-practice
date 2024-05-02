f746f5842967336af3d100e6e7d5069a9c276e29
Mykola Syniuha <mykola.synuiha@true-skin.de>
2024-05-02 22:32:39 +0200

    full project

---
+++ apps/orders/src/orders.service.ts
@@ -0,0 +1,39 @@
+import { Inject, Injectable } from '@nestjs/common';
+import { CreateOrderRequest } from './dto/create-order.request';
+import { OrdersRepository } from './orders.repository';
+import { BILLING_SERVICE } from './constants/services';
+import { ClientProxy } from '@nestjs/microservices';
+import { lastValueFrom } from 'rxjs';
+
+@Injectable()
+export class OrdersService {
+  constructor(
+    private readonly ordersRepository: OrdersRepository,
+    @Inject(BILLING_SERVICE) private readonly billingClient: ClientProxy,
+  ) {}
+
+  async createOrder(request: CreateOrderRequest, authentication: string) {
+    const session = await this.ordersRepository.startTransaction();
+    try {
+      const order = await this.ordersRepository.create(request, { session });
+
+      await lastValueFrom(
+        this.billingClient.emit('order_created', {
+          request,
+          Authentication: authentication,
+        }),
+      );
+
+      await session.commitTransaction();
+
+      return order;
+    } catch (err) {
+      await session.abortTransaction();
+      throw err;
+    }
+  }
+
+  async getOrders() {
+    return this.ordersRepository.find({});
+  }
+}
